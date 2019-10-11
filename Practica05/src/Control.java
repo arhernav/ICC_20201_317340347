2cffbb5a03ecf9ca75f2f7e4184a94ed1124c05f
Armando Hernández Navarro <Arhernav@localhost.localdomain>
2019-10-11 17:08:35 -0500

    Documentacion faltante

--- Practica05/src/Control.java
+++ Practica05/src/Control.java
@@ -2,9 +2,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
+ *Clase control
+ *
  *Clase que contiene metodos aplicables a arreglos
+ *
  *@author Armando Hernández
  *
+ *@date 11/10/19
  */
 
 public class Control{
@@ -12,6 +16,7 @@ public class Control{
     /**
      *Te permite saber si la palabra recibida es un palindromo o no
      *@param Palabra. recibe una paabra
+     *@return Regresa true si es un palindromo, false en otro caso
      */
     public static boolean problema1(String palabra){
 	String inversa = "";
@@ -24,11 +29,37 @@ public class Control{
 	    return false;
 	}
     }
+
+    /**
+     *Te indica si hay dos numeros en el arreglo que sumen un numero seleccionado
+     *
+     *@param K. Recibe un numero K y busca en el arreglo si existen dos numeros que sumen K
+     *
+     *@return Regresa true si en el arreglo existen dos numeros que sumen K. False en otro caso
+     */
     
     public static boolean problema2(int[] arreglo, int k){
+	int aux = 0;
+	for(int i = 0; i < arreglo.length; i++){
+	    for(int j = 0; j < arreglo.length; i++){
+		aux = arreglo[i] + arreglo[j];
+		if(aux == k){
+		    return true;
+		}else{
+		    aux = 0;
+		}
+	    }
+	}
         return false;
     }
     
+    /**
+     *Recibe un arreglo ordenado y devuelve el mismo arreglo pero ordenado
+     *
+     *@param arreglo. recibe un arreglo desordenado
+     *
+     *@return Devuelve un arreglo ordenado
+     */
     public static void problema3(int[] arreglo){
 	for(int i = 0; i< arreglo.length-1; i++){
 	    for(int d = 0; d < arreglo.length -i -1; d++){
@@ -40,11 +71,25 @@ public class Control{
 	    }
 	}
     }
-    
+
+    /**
+     *Recibe un arreglo ordenado y un numero. Regresa true si el numero recibido se encuentra en el arreglo, false en otro caso
+     *
+     *@param Recibe un arreglo ordenado y un numero 
+     *
+     *@return Regresa true si el numero recibido se encuentra en el arreglo ordenado
+     */
     public static boolean problema4(int[] arreglo, int buscando){
         return true;
     }
 
+    /**
+     *Recibe un numero e indica si el numero es primo o no
+     *
+     *@param N. Recibe un numero N e indica si este es primo o no
+     *
+     *@return Regresa true si el numero recibido es primo, false en otro caso
+     */
     public static boolean problema5(int n){
         if(n == 1) return false;
         for(int i = 2; i<n;i++){
@@ -52,7 +97,10 @@ public class Control{
         }
         return true;
     }
-    
+
+    /**
+     *Recibe un numero N y regresa una liste con todos los numeros primos menores a N
+     */
     //public static int[] problema6(int n){
 	
     //}
